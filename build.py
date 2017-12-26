995b0952dc2a3c3ff04d8a1e2de8e9b932837192
memsharded <james@conan.io>
2017-12-26 11:08:17 +0100

    updating CI

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="testing")
-    builder.add_common_builds()
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name="TBB:shared")
     builder.run()
