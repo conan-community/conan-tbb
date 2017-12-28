5171f5008977a572620c557f6f455749bec00857
memsharded <james@conan.io>
2017-12-28 21:53:38 +0100

    1.0.0b3 and static in linux

--- build.py
+++ build.py
@@ -3,5 +3,10 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(shared_option_name="TBB:shared")
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if settings["os"] != "Windows" or options["shared"] != "False":
+             filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
