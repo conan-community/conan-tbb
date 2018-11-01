f32c911d45632a1a99f884e4c9e7471e989bc157
Uilian Ries <uilianries@gmail.com>
2018-11-01 19:02:11 -0300

    #98 Fix build script
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    shared_option_name = False platform.system() == "Windows" else "TBB:shared"
+    shared_option_name = False if platform.system() == "Windows" else "TBB:shared"
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name=shared_option_name)
     builder.run()
