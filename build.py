a9f57e984c267be033475ef3e4dce770a055555b
Uilian Ries <uilianries@gmail.com>
2018-11-03 18:34:00 -0300

    #98 Include C++11 [build=outdated]
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -7,6 +7,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     shared_option_name = False if platform.system() == "Windows" else "TBB:shared"
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(pure_c=False)
     builder.add_common_builds(shared_option_name=shared_option_name)
     builder.run()
